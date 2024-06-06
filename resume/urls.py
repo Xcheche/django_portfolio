9a81699eacc4907fd6bc5866cda6ea441f990b1d
xcheche <chekwubechukwu23@gmail.com>
2024-06-06 14:12:27 +0100

    almost done

--- resume/urls.py
+++ resume/urls.py
@@ -5,4 +5,7 @@ app_name = "resume"
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
+    path("projects/", views.projects, name="projects"),
+    path("experience/", views.experience, name="experience"),
+    path("certificates/", views.certificates, name="certificates"),
 ]
--- resume/views.py
+++ resume/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
@@ -9,3 +10,61 @@ def home(request):
 
 def about(request):
     return render(request, "about.html")
+
+
+def projects(request):
+    projects_show = [
+        {
+            "title": "Ecommerce",
+            "path": "images/projects_img/ecom.jpeg",
+        },
+        {
+            "title": "Salon",
+            "path": "images/projects_img/hairsalon.jpeg",
+        },
+        {
+            "title": "Portfolio",
+            "path": "images/projects_img/portfolio_project.jpeg",
+        },
+    ]
+    return render(request, "projects.html", {"projects_show": projects_show})
+
+
+def experience(request):
+    experience = [
+        {
+            "Company": "Google",
+            "Position": "Software Engineer",
+            "Duration": "2020-present",
+        },
+        {
+            "Company": "Microsoft",
+            "Position": "Django Developer",
+            "Duration": "2020-2021",
+        },
+        {
+            "Company": "Arco",
+            "Position": "Django Developer",
+            "Duration": "2020-2021",
+        },
+        {
+            "Company": "Facebook",
+            "Position": "Django Developer",
+            "Duration": "2020-2021",
+        },
+        {
+            "Company": "Facebook",
+            "Position": "Django Developer",
+            "Duration": "2020-2021",
+        },
+        {
+            "Company": "Facebook",
+            "Position": "Django Developer",
+            "Duration": "2020-2021",
+        },
+    ]
+    return render(request, "experience.html", {"experience": experience})
+
+
+def certificates(request):
+    return render(request, "certificates.html")
